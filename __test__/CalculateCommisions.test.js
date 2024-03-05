58fc47be73ecdeb82219406483cbf8f6c137d5cc
JCarreiro1 <63753146+JCarreiro1@users.noreply.github.com>
2024-03-05 17:09:18 +0000

    added test cases

---
+++ __test__/CalculateCommisions.test.js
@@ -0,0 +1,46 @@
+const  calculateCommissionFunction  = require('../CalculateComission');
+
+describe('calculateCommissionFunction', () => {
+    test('commission for total sales less than or equal to 1000', () => {
+        const [totalSales, commission] = calculateCommissionFunction(10, 10, 10);
+
+        expect(totalSales).toBe(1000);
+        expect(commission).toBe(95.5);
+
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        const [totalSales, commission] = calculateCommissionFunction(10, 20, 30);
+        
+        expect(totalSales).toBe(1800);
+        expect(commission).toBe(263.25);
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        expect(calculateCommissionFunction(100, 150, 200)).toBe("Sales quantities exceed maximum limits.");
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        expect(calculateCommissionFunction(100, 150, 200)).toBe("Sales quantities exceed maximum limits.");
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        expect(calculateCommissionFunction(100, 150, 200)).toBe("Sales quantities exceed maximum limits.");
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        expect(calculateCommissionFunction(100, 150, 200)).toBe("Sales quantities exceed maximum limits.");
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        expect(calculateCommissionFunction(100, 150, 200)).toBe("Sales quantities exceed maximum limits.");
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        expect(calculateCommissionFunction(100, 150, 200)).toBe("Sales quantities exceed maximum limits.");
+    })
+
+    test('commission for total sales less than or equal to 1000', () => {
+        expect(calculateCommissionFunction(100, 150, 200)).toBe("Sales quantities exceed maximum limits.");
+    })
+});
